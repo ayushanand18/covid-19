acf54bacd51fabfe31a66493194415130c62ed0e
Ayush Anand <kingwarrior880@gmail.com>
2020-05-09 10:30:14 +0000

    Feat: Dynamic routing instead of static Desc: This commit changes dynamic routing from static serving of files

--- app.js
+++ app.js
@@ -7,16 +7,19 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var path = require('path');
 
 // cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
 
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
