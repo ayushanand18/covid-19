40d87e14c7e2da2cbe5b75f79e9e424d84e76217
Ayush Anand <kingwarrior880@gmail.com>
2020-05-16 17:55:56 +0000

    Feat: Working on blockchain architecture
    Desc: We are still working on the architecture of the blockchain.

--- blockchain.js
+++ blockchain.js
@@ -1,8 +1,25 @@
 // the script for blockchain app here
 
-module.exports = {
-    main: function(){
-        var status = "Caught Up! We are still developing."
-        return status
-    }
+function blockchain(){
+        this.chain=[]
+        this.pending=[]
+};
+blockchain.prototype.createBlock=function(nonce, prevHash, hash){
+        const newBlock = {
+            index:this.chain.length+1,
+            timestamp:Date.now(),
+            ventilators:this.pending,
+            nonce:nonce,
+            prevHash:prevHash,
+            hash:hash
+        }
+        this.pending=[]
+        this.chain.push(newBlock);
 }
+blockchain.prototype.hash= function(prevHash, blockData, nonce){
+    hash = prevHash+ JSON.stringify(blockData)+ nonce.toString();
+    return hash;
+}
+
+
+module.exports=blockchain();
